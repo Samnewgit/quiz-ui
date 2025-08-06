4a46b6fe959186f9cd1fd38d89af9665916455c8
Samnewgit <samkapadia88@gmail.com>
2025-08-06 18:49:03 +0530

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import QuizContainer from './components/QuizContainer';
+
+function App() {
+  return (
+    <main className="bg-gray-100 min-h-screen w-full flex items-center justify-center p-4 font-sans">
+      <QuizContainer />
+    </main>
+  );
+}
+
+export default App;
