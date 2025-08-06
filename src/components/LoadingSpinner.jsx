75f70ccfc1496a8853a7bdd009358d8bc561a161
Samnewgit <samkapadia88@gmail.com>
2025-08-06 19:11:58 +0530

    Update LoadingSpinner.jsx

--- src/components/LoadingSpinner.jsx
+++ src/components/LoadingSpinner.jsx
@@ -1,4 +1,3 @@
-// src/components/LoadingSpinner.jsx
 import React from 'react';
 import { FiLoader } from 'react-icons/fi';
 
@@ -10,21 +9,3 @@ const LoadingSpinner = ({ text }) => (
 );
 
 export default LoadingSpinner;
-
-// src/components/ErrorMessage.jsx
-import React from 'react';
-import { FiAlertTriangle } from 'react-icons/fi';
-
-const ErrorMessage = ({ message }) => (
-  <div className="bg-red-100 border-l-4 border-red-500 text-red-700 p-4 mb-4 rounded-r-lg" role="alert">
-    <div className="flex items-center">
-      <FiAlertTriangle className="mr-3" />
-      <div>
-        <p className="font-bold">An error occurred</p>
-        <p>{message}</p>
-      </div>
-    </div>
-  </div>
-);
-
-export default ErrorMessage;
